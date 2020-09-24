cf268074f19875c93db5464e20d99f6a4a723b8d
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 12:34:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,54 +16,45 @@ import math
 import os
 import random
 
-import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.optim as optim
+import torch.utils.data
+import torch.utils.data.distributed
 from torch.cuda import amp
-from torch.utils.data.dataloader import DataLoader
 
+from fsrcnn_pytorch import DatasetFromFolder
 from fsrcnn_pytorch import FSRCNN
-from fsrcnn_pytorch import TrainDataset
-from fsrcnn_pytorch import ValDataset
+from fsrcnn_pytorch import progress_bar
 
 parser = argparse.ArgumentParser(description="Fast Super Resolution CNN.")
-parser.add_argument("--train-file", type=str,
-                    default="./data/train/BSDS300_X4.h5",
-                    help="Path to train datasets. "
-                         "(default:`./data/train/BSDS300_X4.h5`)")
-parser.add_argument("--eval-file", type=str,
-                    default="./data/val/Set5_X4.h5",
-                    help="Path to eval datasets. "
-                         "(default:`./data/val/Set5_X4.h5`)")
+parser.add_argument("--dataroot", type=str, default="./data/DIV2K",
+                    help="Path to datasets. (default:`./data/DIV2K`)")
 parser.add_argument("-j", "--workers", default=0, type=int, metavar="N",
                     help="Number of data loading workers. (default:0)")
 parser.add_argument("--epochs", default=200, type=int, metavar="N",
                     help="Number of total epochs to run. (default:200)")
-parser.add_argument("-b", "--batch-size", default=64, type=int,
+parser.add_argument("--image-size", type=int, default=256,
+                    help="Size of the data crop (squared assumed). (default:256)")
+parser.add_argument("-b", "--batch-size", default=16, type=int,
                     metavar="N",
-                    help="mini-batch size (default: 64), this is the total "
+                    help="mini-batch size (default: 16), this is the total "
                          "batch size of all GPUs on the current node when "
                          "using Data Parallel or Distributed Data Parallel.")
-parser.add_argument("--lr", type=float, default=0.001,
-                    help="Learning rate. (default:0.001)")
-parser.add_argument("--scale-factor", type=int, default=4,
+parser.add_argument("--lr", type=float, default=0.01,
+                    help="Learning rate. (default:0.01)")
+parser.add_argument("--scale-factor", type=int, default=4, choices=[2, 3, 4],
                     help="Low to high resolution scaling factor. (default:4).")
+parser.add_argument("-p", "--print-freq", default=5, type=int,
+                    metavar="N", help="Print frequency. (default:5)")
 parser.add_argument("--cuda", action="store_true", help="Enables cuda")
-parser.add_argument("--outf", default="./outputs",
-                    help="folder to output images. (default:`./outputs`).")
 parser.add_argument("--weights", default="",
                     help="Path to weights (to continue training).")
-parser.add_argument("--manualSeed", type=int,
-                    help="Seed for initializing training. (default:none)")
+parser.add_argument("--manualSeed", type=int, default=0,
+                    help="Seed for initializing training. (default:0)")
 
 args = parser.parse_args()
 print(args)
-
-try:
-    os.makedirs(args.outf)
-except OSError:
-    pass
 
 try:
     os.makedirs("weights")
@@ -79,49 +70,53 @@ torch.manual_seed(args.manualSeed)
 cudnn.benchmark = True
 
 if torch.cuda.is_available() and not args.cuda:
-    print("WARNING: You have a CUDA device, "
-          "so you should probably run with --cuda")
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-train_dataset = TrainDataset(args.train_file)
+train_dataset = DatasetFromFolder(f"{args.dataroot}/train",
+                                  image_size=args.image_size,
+                                  scale_factor=args.scale_factor)
+val_dataset = DatasetFromFolder(f"{args.dataroot}/val",
+                                image_size=args.image_size,
+                                scale_factor=args.scale_factor)
+
 train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=args.batch_size,
-                                               shuffle=True, pin_memory=True,
+                                               shuffle=True,
+                                               pin_memory=True,
                                                num_workers=int(args.workers))
-val_dataset = ValDataset(args.eval_file)
 val_dataloader = torch.utils.data.DataLoader(val_dataset,
-                                             batch_size=1,
-                                             shuffle=False, pin_memory=True,
+                                             batch_size=args.batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
                                              num_workers=int(args.workers))
 
 device = torch.device("cuda:0" if args.cuda else "cpu")
 
-model = FSRCNN(scale_factor=args.scale_factor).to(device)
+model = FSRCNN(num_channels=1, scale_factor=args.scale_factor).to(device)
+model.weight_init()
 
 if args.weights:
     model.load_state_dict(torch.load(args.weights, map_location=device))
 
 criterion = nn.MSELoss().to(device)
-optimizer = optim.Adam([
-    {"params": model.first_part.parameters()},
-    {"params": model.mid_part.parameters()},
-    {"params": model.last_part.parameters(), "lr": args.lr * 0.1}
-], lr=args.lr)
+# we use Adam instead of SGD like in the paper, because it's faster
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 75, 100], gamma=0.5)  # lr decay
 
 best_psnr = 0.
 
 # Creates a GradScaler once at the beginning of training.
 scaler = amp.GradScaler()
 
-for epoch in range(0, args.epochs):
+for epoch in range(args.epochs):
     model.train()
-
-    # Train
-    epoch_loss = 0
-    for data in train_dataloader:
+    train_loss = 0.
+    print("\n")
+    print(f"Start train epoch {epoch + 1}.")
+    for iteration, (inputs, target) in enumerate(train_dataloader):
         optimizer.zero_grad()
 
-        inputs = data[0].to(device)
-        target = data[1].to(device)
+        inputs, target = inputs.to(device), target.to(device)
 
         # Runs the forward pass with autocasting.
         with amp.autocast():
@@ -144,28 +139,32 @@ for epoch in range(0, args.epochs):
 
         # Updates the scale for next iteration.
         scaler.update()
+        scheduler.step()
 
-        epoch_loss += loss.item()
+        train_loss += loss.item()
 
-    print(f"Epoch {epoch}. "
-          f"Training loss: {epoch_loss / len(train_dataloader):.6f}")
+        progress_bar(iteration, len(train_dataloader), f"Loss: {train_loss / (iteration + 1):.6f}")
+
+    print(f"Epoch [{epoch + 1}/[{args.epochs}]. Training average loss: {train_loss / len(train_dataloader):.6f}")
 
     # Test
-    avg_psnr = 0
+    print("\n")
+    print(f"Start eval epoch {epoch + 1}.")
+    model.eval()
+    avg_psnr = 0.
     with torch.no_grad():
-        for data in val_dataloader:
-            inputs = data[0].to(device)
-            target = data[1].to(device)
+        for iteration, (inputs, target) in enumerate(val_dataloader):
+            inputs, target = inputs.to(device), target.to(device)
 
-            output = model(inputs).clamp(0.0, 1.0)
-            loss = criterion(output, target)
-            psnr = 10 * math.log10(1 / loss.item())
+            prediction = model(inputs)
+            mse = criterion(prediction, target)
+            psnr = 10 * math.log10(1 / mse.item())
             avg_psnr += psnr
-        print(f"Average PSNR: {avg_psnr / len(val_dataloader):.2f} dB.")
+    print(f"Average PSNR: {avg_psnr / len(val_dataloader):.2f} dB.")
 
     # Save model
-    torch.save(model.state_dict(), f"weights/model_{epoch}.pth")
+    if (epoch + 1) % 20 == 0:
+        torch.save(model.state_dict(), f"weights/fsrcnn_{args.scale_factor}x_epoch_{epoch + 1}.pth")
     if avg_psnr > best_psnr:
         best_psnr = avg_psnr
-        torch.save(model.state_dict(),
-                   f"weights/fsrcnn_X{args.scale_factor}.pth")
+        torch.save(model.state_dict(), f"weights/fsrcnn_{args.scale_factor}x.pth")
