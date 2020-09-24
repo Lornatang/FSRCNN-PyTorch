adc4d432344c5b04e538f24f2e6861b39f8b5d8a
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 12:31:31 +0800

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-wget https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz
-tar -xzvf  BSDS300-images.tgz
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+
+unzip DIV2K_train_HR.zip
+unzip DIV2K_valid_HR.zip
+
+mkdir DIV2K
+
+mv DIV2K_train_HR DIV2K/train
+mv DIV2K_valid_HR DIV2K/val
+
+rm DIV2K_train_HR.zip
+rm DIV2K_valid_HR.zip
