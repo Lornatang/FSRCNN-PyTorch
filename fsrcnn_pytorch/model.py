af0e0e1a525baa37ae6338238a00f29ef3566648
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 15:55:19 +0800

    Update model.py

--- fsrcnn_pytorch/model.py
+++ fsrcnn_pytorch/model.py
@@ -19,37 +19,37 @@ class FSRCNN(nn.Module):
     def __init__(self, num_channels, scale_factor):
         super(FSRCNN, self).__init__()
 
-        # Feature extraction
-        self.first_part = nn.Sequential(
+        self.main = nn.Sequential(
+            # Feature extraction
             nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=5, stride=1, padding=2),
-            nn.PReLU())
+            nn.PReLU(),
 
-        self.mid_part = nn.Sequential(
+            # Channel shrinking
             nn.Conv2d(in_channels=64, out_channels=12, kernel_size=1, stride=1, padding=0),
             nn.PReLU(),
 
+            # Channel mapping
             nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
             nn.PReLU(),
 
+            # Channel expanding
             nn.Conv2d(in_channels=12, out_channels=64, kernel_size=1, stride=1, padding=0),
-            nn.PReLU()
+            nn.PReLU(),
+
+            # Deconvolution
+            nn.ConvTranspose2d(in_channels=64, out_channels=num_channels, kernel_size=9,
+                               stride=scale_factor, padding=3, output_padding=1)
         )
 
-        # Deconvolution
-        self.last_part = nn.ConvTranspose2d(in_channels=64, out_channels=num_channels, kernel_size=9,
-                                            stride=scale_factor, padding=3, output_padding=1)
-
     @amp.autocast()
-    def forward(self, x):
-        out = self.first_part(x)
-        out = self.mid_part(out)
-        out = self.last_part(out)
+    def forward(self, inputs):
+        out = self.main(inputs)
         return out
 
-    def weight_init(self, mean=0.0, std=0.02):
+    def weight_init(self, mean=0.0, std=0.2):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 m.weight.data.normal_(mean, std)
