ee0be31359b0129edc27e709e12791fbe2bfc4e6
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 12:32:43 +0800

    Update test_image.py

--- test_image.py
+++ test_image.py
@@ -13,96 +13,63 @@
 # ==============================================================================
 import argparse
 
-import cv2
 import numpy as np
-import torch
 import torch.backends.cudnn as cudnn
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
 from PIL import Image
-from sewar.full_ref import msssim
-from sewar.full_ref import sam
-from sewar.full_ref import vifp
 
 from fsrcnn_pytorch import FSRCNN
-from fsrcnn_pytorch import cal_mse
-from fsrcnn_pytorch import cal_niqe
-from fsrcnn_pytorch import cal_psnr
-from fsrcnn_pytorch import cal_rmse
-from fsrcnn_pytorch import cal_ssim
-from fsrcnn_pytorch import convert_ycbcr_to_rgb
-from fsrcnn_pytorch import preprocess
 
 parser = argparse.ArgumentParser(description="Fast Super Resolution CNN.")
 parser.add_argument("--file", type=str, default="./assets/baby.png",
-                    help="The file address where the image needs "
-                         "to be processed. (default: `./assets/baby.png`).")
-parser.add_argument("--weights", type=str, default="weights/fsrcnn_X4.pth",
-                    required=True, help="Generator model name.  "
-                                        "(default:`weights/fsrcnn_X4.pth`)")
+                    help="Test low resolution image name. "
+                         "(default:`./assets/baby.png`)")
+parser.add_argument("--weights", type=str, default="weights/fsrcnn_4x.pth",
+                    help="Generator model name.  "
+                         "(default:`weights/fsrcnn_4x.pth`)")
 parser.add_argument("--cuda", action="store_true", help="Enables cuda")
-parser.add_argument("--scale-factor", type=int, default=4, choices=[2, 3, 4],
-                    help="Image scaling ratio. (default: `4`).")
-parser.add_argument("--manualSeed", type=int,
-                    help="Seed for initializing training. (default:none)")
+parser.add_argument("--scale-factor", default=4, type=int, choices=[2, 3, 4],
+                    help="Super resolution upscale factor. (default:4)")
 
 args = parser.parse_args()
 print(args)
 
 cudnn.benchmark = True
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model = FSRCNN(scale_factor=args.scale_factor).to(device)
-model.load_state_dict(torch.load(args.weights))
+if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have a CUDA device, "
+          "so you should probably run with --cuda")
 
-model.eval()
+device = torch.device("cuda:0" if args.cuda else "cpu")
 
-image = Image.open(args.file).convert("RGB")
+# create model
+model = FSRCNN(num_channels=1, scale_factor=args.scale_factor).to(device)
 
-image_width = (image.width // args.scale) * args.scale
-image_height = (image.height // args.scale) * args.scale
+# Load state dicts
+model.load_state_dict(torch.load(args.weights, map_location=device))
 
-hr = image.resize((image_width, image_height),
-                  resample=Image.BICUBIC)
-lr = hr.resize((hr.width // args.scale, hr.height // args.scale),
-               resample=Image.BICUBIC)
-bicubic = lr.resize((lr.width * args.scale, lr.height * args.scale),
-                    resample=Image.BICUBIC)
+# Open image
+image = Image.open(args.file).convert("YCbCr")
+image_width = int(image.size[0] * args.scale_factor)
+image_height = int(image.size[1] * args.scale_factor)
+image = image.resize((image_width, image_height), Image.BICUBIC)
+y, cb, cr = image.split()
 
-lr, _ = preprocess(lr, device)
-hr, _ = preprocess(hr, device)
-_, ycbcr = preprocess(bicubic, device)
+preprocess = transforms.ToTensor()
+inputs = preprocess(y).view(1, -1, y.size[1], y.size[0])
 
-with torch.no_grad():
-    preds = model(lr).clamp(0.0, 1.0)
+inputs = inputs.to(device)
 
-preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+out = model(inputs)
+out = out.cpu()
+out_image_y = out[0].detach().numpy()
+out_image_y *= 255.0
+out_image_y = out_image_y.clip(0, 255)
+out_image_y = Image.fromarray(np.uint8(out_image_y[0]), mode="L")
 
-output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose(
-    [1, 2, 0])
-output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(
-    np.uint8)
-output = Image.fromarray(output)
-output.save("result.png")
-
-# Evaluate performance
-src_img = cv2.imread("result.png")
-dst_img = cv2.imread(args.file)
-dst_img = cv2.resize(dst_img, (src_img.shape[1], src_img.shape[0]),
-                     interpolation=cv2.INTER_CUBIC)
-
-mse_value = cal_mse(src_img, dst_img)
-rmse_value = cal_rmse(src_img, dst_img)
-psnr_value = cal_psnr(src_img, dst_img)
-ssim_value = cal_ssim(src_img, dst_img)
-ms_ssim_value = msssim(src_img, dst_img)
-niqe_value = cal_niqe("result.png")
-sam_value = sam(src_img, dst_img)
-vif_value = vifp(src_img, dst_img)
-
-print(f"MSE: {mse_value:.2f}\n"
-      f"RMSE: {rmse_value:.2f}\n"
-      f"PSNR: {psnr_value:.2f}\n"
-      f"SSIM: {ssim_value:.4f}\n"
-      f"MS-SSIM: {ms_ssim_value:.4f}\n"
-      f"NIQE: {niqe_value:.2f}\n"
-      f"SAM: {sam_value:.4f}\n"
-      f"VIF: {vif_value:.4f}")
+out_img_cb = cb.resize(out_image_y.size, Image.BICUBIC)
+out_img_cr = cr.resize(out_image_y.size, Image.BICUBIC)
+out_img = Image.merge("YCbCr", [out_image_y, out_img_cb, out_img_cr]).convert("RGB")
+# before converting the result in RGB
+out_img.save(f"fsrcnn_{args.scale_factor}x.png")
