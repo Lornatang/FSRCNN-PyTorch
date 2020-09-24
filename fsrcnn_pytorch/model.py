2425c814ca36a10b5782ca6dba477663c470411a
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 13:18:00 +0800

    Update model.py

--- fsrcnn_pytorch/model.py
+++ fsrcnn_pytorch/model.py
@@ -11,48 +11,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import math
-
-from torch import nn
+import torch.nn as nn
 from torch.cuda import amp
 
 
 class FSRCNN(nn.Module):
-    def __init__(self, scale_factor, num_channels=1, d=56, s=12, m=4):
+    def __init__(self, num_channels, scale_factor):
         super(FSRCNN, self).__init__()
+
+        # Feature extraction
         self.first_part = nn.Sequential(
-            nn.Conv2d(num_channels, d, kernel_size=5, padding=5 // 2),
-            nn.PReLU(d)
+            nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.PReLU())
+
+        self.mid_part = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=12, kernel_size=1, stride=1, padding=0),
+            nn.PReLU(),
+
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1),
+            nn.PReLU(),
+
+            nn.Conv2d(in_channels=12, out_channels=64, kernel_size=1, stride=1, padding=0),
+            nn.PReLU()
         )
-        self.mid_part = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]
-        for _ in range(m):
-            self.mid_part.extend(
-                [nn.Conv2d(s, s, kernel_size=3, padding=3 // 2), nn.PReLU(s)])
-        self.mid_part.extend([nn.Conv2d(s, d, kernel_size=1), nn.PReLU(d)])
-        self.mid_part = nn.Sequential(*self.mid_part)
-        self.last_part = nn.ConvTranspose2d(d, num_channels, kernel_size=9,
-                                            stride=scale_factor, padding=9 // 2,
-                                            output_padding=scale_factor - 1)
 
-        self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.first_part:
-            if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(
-                    2 / (m.out_channels * m.weight.data[0][0].numel())))
-                nn.init.zeros_(m.bias.data)
-        for m in self.mid_part:
-            if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight.data, mean=0.0, std=math.sqrt(
-                    2 / (m.out_channels * m.weight.data[0][0].numel())))
-                nn.init.zeros_(m.bias.data)
-        nn.init.normal_(self.last_part.weight.data, mean=0.0, std=0.001)
-        nn.init.zeros_(self.last_part.bias.data)
+        # Deconvolution
+        self.last_part = nn.ConvTranspose2d(in_channels=64, out_channels=num_channels, kernel_size=9,
+                                            stride=scale_factor, padding=3, output_padding=1)
 
     @amp.autocast()
     def forward(self, x):
-        x = self.first_part(x)
-        x = self.mid_part(x)
-        x = self.last_part(x)
-        return x
+        out = self.first_part(x)
+        out = self.mid_part(out)
+        out = self.last_part(out)
+        return out
+
+    def weight_init(self, mean=0.0, std=0.02):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(mean, std)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            if isinstance(m, nn.ConvTranspose2d):
+                m.weight.data.normal_(0.0, 0.0001)
+                if m.bias is not None:
+                    m.bias.data.zero_()
