e5beddac7886dcd12589888963cea2c4e1777840
Changyu <liuchangyu1111@gmail.com>
2020-09-29 08:02:52 +0800

    update

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start downloading pre training model..."
-wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.1/fsrcnn_2x.pth
-wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.1/fsrcnn_3x.pth
-wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.1/fsrcnn_4x.pth
+wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.2/fsrcnn_2x.pth
+wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.2/fsrcnn_3x.pth
+wget https://github.com/Lornatang/FSRCNN-PyTorch/releases/download/1.2/fsrcnn_4x.pth
 echo "All pre training models have been downloaded!"
