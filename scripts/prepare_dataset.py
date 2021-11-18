452bba7aa4054a3c43603417f56152e79b78e1ff
Goodfellow <liuchangyu1111@gmail.com>
2021-11-19 00:24:38 +0800

    Update

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -54,15 +54,15 @@ def main(args):
                 # Save all images
                 crop_image.save(f"{new_image_dir}/{file_name.split('.')[-2]}_{pos_x}_{pos_y}.{file_name.split('.')[-1]}")
 
-    shutil.rmtree(new_image_dir)
+    shutil.rmtree(raw_image_dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare database scripts.")
     parser.add_argument("--inputs_dir", type=str, default="TG191/original", help="Path to input image directory. (Default: `TG191/original`)")
     parser.add_argument("--output_dir", type=str, default="TG191", help="Path to generator image directory. (Default: `TG191`)")
-    parser.add_argument("--image_size", type=int, default=33, help="Low-resolution image size from raw image. (Default: 33)")
-    parser.add_argument("--step", type=int, default=14, help="Crop image similar to sliding window.  (Default: 14)")
+    parser.add_argument("--image_size", type=int, default=36, help="Low-resolution image size from raw image. (Default: 36)")
+    parser.add_argument("--step", type=int, default=36, help="Crop image similar to sliding window.  (Default: 36)")
     args = parser.parse_args()
 
     main(args)
