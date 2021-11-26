3ca16ade7f47f7be7a1c6c590a70a15661673a4c
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-26 14:07:48 +0800

    Update prepare_dataset.py

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -19,7 +19,7 @@ from PIL import Image
 from tqdm import tqdm
 
 
-def main(args):
+def main():
     raw_image_dir = f"{args.output_dir}/temp"
     new_image_dir = f"{args.output_dir}/train"
 
@@ -31,17 +31,20 @@ def main(args):
     os.makedirs(new_image_dir)
 
     # Carry out data enhancement processing on the data set in the temp catalog in turn
-    for file_name in os.listdir(args.inputs_dir):
+    file_names = os.listdir(args.inputs_dir)
+    for file_name in tqdm(file_names, total=len(file_names)):
         raw_image = Image.open(f"{args.inputs_dir}/{file_name}")
 
         index = 0
-        for scale_ratio in [1.0, 0.9, 0.8, 0.7, 0.6]:
-            for rotate_angle in [0, 90, 180, 270]:
+        for scale_ratio in [1.0, 0.9, 0.7, 0.5]:
+            for rotate_angle in [0, 90, 180]:
                 # Process HR image
-                new_image = raw_image.resize((int(raw_image.width * scale_ratio), int(raw_image.height * scale_ratio)), Image.BICUBIC)
-                new_image = new_image.rotate(rotate_angle, expand=True)
-                new_image.save(f"{raw_image_dir}/{file_name.split('.')[-2]}_{index:04d}.{file_name.split('.')[-1]}")
+                hr_image = raw_image.resize((int(raw_image.width * scale_ratio), int(raw_image.height * scale_ratio)), Image.BICUBIC) if scale_ratio != 1.0 else raw_image
+                hr_image = hr_image.rotate(rotate_angle) if rotate_angle != 0 else hr_image
+                # Save all images
+                hr_image.save(f"{raw_image_dir}/{file_name.split('.')[-2]}_{index:04d}.{file_name.split('.')[-1]}")
                 index += 1
+    print("Data augment successful.")
 
     file_names = os.listdir(raw_image_dir)
     for file_name in tqdm(file_names, total=len(file_names)):
@@ -55,14 +58,15 @@ def main(args):
                 crop_image.save(f"{new_image_dir}/{file_name.split('.')[-2]}_{pos_x}_{pos_y}.{file_name.split('.')[-1]}")
 
     shutil.rmtree(raw_image_dir)
+    print("Data split successful.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare database scripts.")
     parser.add_argument("--inputs_dir", type=str, default="TG191/original", help="Path to input image directory. (Default: `TG191/original`)")
-    parser.add_argument("--output_dir", type=str, default="TG191/FSRCNN", help="Path to generator image directory. (Default: `TG191/FSRCNN`)")
+    parser.add_argument("--output_dir", type=str, default="TG191", help="Path to generator image directory. (Default: `TG191`)")
     parser.add_argument("--image_size", type=int, default=33, help="Low-resolution image size from raw image. (Default: 33)")
     parser.add_argument("--step", type=int, default=14, help="Crop image similar to sliding window.  (Default: 14)")
     args = parser.parse_args()
 
-    main(args)
+    main()
