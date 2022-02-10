e79e5e0ae56d240f0a2726d7382fbc037b2edcae
Lorna <liuchangyu1111@gmail.com>
2022-02-10 22:22:46 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -2,7 +2,7 @@ import os
 
 # Prepare dataset
 os.system("python ./create_multiscale_dataset.py --images_dir ../data/TG191/original --output_dir ../data/TG191/FSRCNN/original")
-os.system("python ./prepare_dataset.py --images_dir ../data/TG191/FSRCNN/original --output_dir ../data/TG191/FSRCNN/train --image_size 32 --step 16")
+os.system("python ./prepare_dataset.py --images_dir ../data/TG191/FSRCNN/original --output_dir ../data/TG191/FSRCNN/train --image_size 32 --step 16 --num_workers 10")
 
 # Split train and valid
 os.system("python ./split_train_valid_dataset.py --train_images_dir ../data/TG191/FSRCNN/train --valid_images_dir ../data/TG191/FSRCNN/valid --valid_samples_ratio 0.1")
